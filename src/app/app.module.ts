a244845b04319e585b29ab2a115e2583b3aca21c
David Lasry <david.l@tomadox.co.uk>
2019-07-23 17:07:45 +0300

    create mocks api requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, SearchBarComponent],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/mocks/currentWeather.ts
@@ -0,0 +1,29 @@
+// GET /currentconditions/v1/213225?apikey=MVu5tD8P1Pzp0kGZfdIhuIVuqGrxtYns HTTP/1.1
+
+export const currentWeather = [
+  {
+    LocalObservationDateTime: '2019-07-23T16:40:00+03:00',
+    EpochTime: 1563889200,
+    WeatherText: 'Sunny',
+    WeatherIcon: 1,
+    HasPrecipitation: false,
+    PrecipitationType: null,
+    IsDayTime: true,
+    Temperature: {
+      Metric: {
+        Value: 29.8,
+        Unit: 'C',
+        UnitType: 17
+      },
+      Imperial: {
+        Value: 86,
+        Unit: 'F',
+        UnitType: 18
+      }
+    },
+    MobileLink:
+      'http://m.accuweather.com/en/il/jerusalem/213225/current-weather/213225?lang=en-us',
+    Link:
+      'http://www.accuweather.com/en/il/jerusalem/213225/current-weather/213225?lang=en-us'
+  }
+];
