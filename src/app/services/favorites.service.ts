2c79ab2947106148f27acb5e773fff92fc936e27
Lasry David <lasry.david@gmail.com>
2019-08-03 20:42:53 +0300

    https secure requests to accuweather

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -7,23 +7,20 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class FavoritesService {
-  private API_BASE_URL = 'http://dataservice.accuweather.com';
+  private API_BASE_URL = 'https://dataservice.accuweather.com';
   private API_KEY = '9Qj8JUuV4jSur8p2FfosAH0yTZmByj4p';
 
   constructor(private http: HttpClient) {}
 
   getFiveDaysPredictions(cityID) {
-    // console.log('SERVICE getFiveDaysPredictions cityID:', cityID);
     return this.http.get<any>(
       `${this.API_BASE_URL}/forecasts/v1/daily/5day/${cityID}?apikey=${
         this.API_KEY
       }&metric=true`
     );
-    // .pipe(map(movies => movies.results));
   }
 
   getCurrentWeather(cityID) {
-    // console.log('SERVICE getCurrentWeather cityID:', cityID);
     return this.http.get<any>(
       `${this.API_BASE_URL}/currentconditions/v1/${cityID}?apikey=${
         this.API_KEY
@@ -32,7 +29,6 @@ export class FavoritesService {
   }
 
   searchCity(textInput) {
-    console.log('textInput:', textInput);
     return this.http.get<any>(
       `${this.API_BASE_URL}/locations/v1/cities/autocomplete?apikey=${
         this.API_KEY
